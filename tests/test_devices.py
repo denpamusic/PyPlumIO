3e3cd9cdcc0a9439ff2e76649322356c3e0f2690
denpamusic <denpa@denpa.pro>
2022-01-13 06:17:01 +0300

    Fix tests.

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -1,7 +1,7 @@
 import pytest
 
 from pyplumio.constants import MODULE_A
-from pyplumio.devices import EcoMAX
+from pyplumio.devices import EcoMAX, Parameter
 
 _test_data = {
     "modeString": "Heating",
@@ -41,20 +41,28 @@ _test_parameters = {
 
 
 @pytest.fixture
-def ecomax_with_data():
-    ecomax = EcoMAX()
+def ecomax() -> EcoMAX:
+    return EcoMAX()
+
+
+@pytest.fixture
+def ecomax_with_data(ecomax) -> EcoMAX:
     ecomax.set_data(_test_data)
     ecomax.set_parameters(_test_parameters)
     return ecomax
 
 
 @pytest.fixture
-def ecomax_without_data():
-    return EcoMAX()
+def parameter() -> Parameter:
+    return Parameter(name="AUTO_SUMMER", value=1, min_=0, max_=1)
 
 
-def test_has_no_data(ecomax_without_data: EcoMAX):
-    assert not ecomax_without_data.has_data()
+def test_has_no_data(ecomax: EcoMAX):
+    assert not ecomax.has_data()
+
+
+def test_has_no_parameters(ecomax: EcoMAX):
+    assert not ecomax.has_parameters()
 
 
 def test_has_data(ecomax_with_data: EcoMAX):
@@ -71,16 +79,64 @@ def test_get_attr_from_data(ecomax_with_data: EcoMAX):
 
 
 def test_get_parameters(ecomax_with_data: EcoMAX):
-    assert ecomax_with_data.parameters["AUTO_SUMMER"]["value"] == 1
+    assert ecomax_with_data.parameters["AUTO_SUMMER"] == 1
+
+
+def test_get_attr_from_parameters(ecomax_with_data: EcoMAX):
+    assert ecomax_with_data.auto_summer == 1
+
+
+def test_set_attr_from_parameters(ecomax_with_data: EcoMAX):
+    ecomax_with_data.auto_summer = 0
+    assert ecomax_with_data.auto_summer == 0
+
+
+def test_set_attr_from_parameters_out_of_range(ecomax_with_data: EcoMAX):
+    ecomax_with_data.auto_summer = 39
+    assert ecomax_with_data.auto_summer == 1
+
+
+def test_changed_parameters(ecomax_with_data: EcoMAX):
+    ecomax_with_data.auto_summer = 0
+    assert ecomax_with_data.changes[0].name == "AUTO_SUMMER"
 
 
 def test_has_parameters(ecomax_with_data: EcoMAX):
     assert ecomax_with_data.has_parameters()
 
 
-def test_has_no_parameters(ecomax_without_data: EcoMAX):
-    assert not ecomax_without_data.has_parameters()
-
-
 def test_to_str(ecomax_with_data: EcoMAX):
     assert "Software Ver.:  1.1.15" in str(ecomax_with_data)
+
+
+def test_parameter_set(parameter: Parameter):
+    parameter.set(0)
+    assert parameter == 0
+
+
+def test_parameter_set_out_of_range(parameter: Parameter):
+    parameter.set(39)
+    assert parameter == 1
+
+
+def test_parameter_compare(parameter: Parameter):
+    assert parameter == 1
+    assert parameter < 2
+    assert parameter > 0
+    assert 0 <= parameter <= 1
+
+
+def test_parameter__repr__(parameter: Parameter):
+    output = """Parameter(
+    name = AUTO_SUMMER,
+    value = 1,
+    min_ = 0,
+    max_ = 1,
+    changed = False
+)""".strip()
+
+    assert repr(parameter) == output
+
+
+def test_parameter__str__(parameter: Parameter):
+    assert str(parameter) == "AUTO_SUMMER: 1 (range 0 - 1)"
