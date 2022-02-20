2a309eeb532cf27a33d443872adcab0e577bef19
denpamusic <denpa@denpa.pro>
2022-02-20 05:14:52 +0300

    Add device available test.

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,4 +1,5 @@
 from pyplumio import responses
+from pyplumio.constants import WLAN_ENCRYPTION, WLAN_ENCRYPTION_NONE
 from pyplumio.frame import BROADCAST_ADDRESS, ECONET_ADDRESS
 
 
@@ -18,26 +19,66 @@ def test_responses_type():
         assert isinstance(frame, response)
 
 
+_program_version_data = {
+    "version": "1.0.0",
+    "struct_tag": b"\xFF\xFF",
+    "struct_version": 5,
+    "device_id": b"\x7A\x00",
+    "processor_signature": b"\x00\x00\x00",
+    "address": 0x56,
+}
+_program_version_bytes = b"\xFF\xFF\x05\x7A\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x56"
+
+
 def test_program_version_create_message():
-    data = {
-        "version": "1.0.0",
-        "struct_tag": b"\xFF\xFF",
-        "struct_version": 5,
-        "device_id": b"\x7A\x00",
-        "processor_signature": b"\x00\x00\x00",
-    }
     frame = responses.ProgramVersion(
-        recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS, data=data
+        recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS, data=_program_version_data
     )
-    message = frame.create_message()
-    assert message == b"\xFF\xFF\x05\x7A\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x56"
+    assert frame.create_message() == _program_version_bytes
 
 
 def test_program_version_parse_message():
     frame = responses.ProgramVersion(recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS)
-    frame.parse_message(b"\xFF\xFF\x05\x7A\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x56")
-    assert frame.data["version"] == "1.0.0"
-    assert frame.data["struct_tag"] == b"\xFF\xFF"
-    assert frame.data["struct_version"] == 5
-    assert frame.data["device_id"] == b"\x7A\x00"
-    assert frame.data["processor_signature"] == b"\x00\x00\x00"
+    frame.parse_message(_program_version_bytes)
+    assert frame.data == _program_version_data
+
+
+_device_available_data = {
+    "eth": {
+        "ip": "192.168.1.2",
+        "netmask": "255.255.255.0",
+        "gateway": "192.168.1.1",
+        "status": True,
+    },
+    "wlan": {
+        "ip": "192.168.2.2",
+        "netmask": "255.255.255.0",
+        "gateway": "192.168.2.1",
+        "encryption": WLAN_ENCRYPTION[WLAN_ENCRYPTION_NONE],
+        "quality": 100,
+        "status": True,
+        "ssid": "tests",
+    },
+    "server": {"status": True},
+}
+_device_available_bytes = (
+    b"\x01\xC0\xA8\x01\x02\xFF\xFF\xFF\x00\xC0\xA8\x01\x01\x01"
+    + b"\xC0\xA8\x02\x02\xFF\xFF\xFF\x00\xC0\xA8\x02\x01\x01\x01\x64\x01\x00\x00"
+    + b"\x00\x00\x05\x74\x65\x73\x74\x73"
+)
+
+
+def test_device_available_create_message():
+    frame = responses.DeviceAvailable(
+        recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS, data=_device_available_data
+    )
+    assert frame.create_message() == _device_available_bytes
+
+
+def test_device_available_parse_message():
+    frame = responses.DeviceAvailable(
+        recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS
+    )
+    frame.parse_message(_device_available_bytes)
+    print(frame.data)
+    assert frame.data == _device_available_data
