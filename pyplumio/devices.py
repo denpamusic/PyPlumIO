0c0779cd461d459e975e68435e0f67f63d72ae67
denpamusic <denpa@denpa.pro>
2022-04-07 16:15:22 +0300

    Move ecoMAX methods to specific class.

--- pyplumio/devices.py
+++ pyplumio/devices.py
@@ -50,12 +50,7 @@ class Device(BaseDevice):
         uid -- device uid string
         password -- device service password
         schema -- device regdata schema
-        _fuel_burned -- amount of fuel burned in kilograms
-        _fuel_burned_timestamp -- timestamp of burned fuel value update
     """
-
-    _fuel_burned: float = 0.0
-    _fuel_burned_timestamp: float = 0.0
 
     def __init__(
         self, data: Dict[str, Any] = None, parameters: Dict[str, List[int]] = None
@@ -72,8 +67,6 @@ class Device(BaseDevice):
         self.__dict__["uid"] = None
         self.__dict__["password"] = None
         self.__dict__["schema"] = []
-        self.__dict__["_fuel_burned"] = 0.0
-        self.__dict__["_fuel_burned_timestamp"] = time.time()
         super().__init__(data, parameters)
 
     def __str__(self) -> str:
@@ -100,13 +93,8 @@ Mixers:
             if name in DEVICE_DATA:
                 self._data[name] = value
 
-        if DATA_FUEL_CONSUMPTION in data:
-            self._set_fuel_burned(data[DATA_FUEL_CONSUMPTION])
-
         if DATA_FRAMES in data:
             self.bucket.fill(data[DATA_FRAMES])
-
-        self._set_boiler_control_parameter()
 
     def set_parameters(self, parameters: Dict[str, List[int]]) -> None:
         """Sets device parameters.
@@ -117,35 +105,6 @@ Mixers:
         for name, parameter in parameters.items():
             if name in DEVICE_PARAMS:
                 self._parameters[name] = Parameter(name, *parameter)
-
-    def _set_boiler_control_parameter(self):
-        """Sets boiler control parameter from device data."""
-        if PARAM_BOILER_CONTROL in self._parameters and isinstance(
-            self._parameters[PARAM_BOILER_CONTROL], Parameter
-        ):
-            self._parameters[PARAM_BOILER_CONTROL].value = int(self.is_on)
-        else:
-            self._parameters[PARAM_BOILER_CONTROL] = Parameter(
-                name=PARAM_BOILER_CONTROL, value=int(self.is_on), min_=0, max_=1
-            )
-
-    def _set_fuel_burned(self, fuel_consumption: float) -> None:
-        """Sets amount of fuel burned since last message.
-
-        Keyword arguments:
-            fuel_consumption -- fuel flow in kg/h
-        """
-        current_timestamp = time.time()
-        seconds_passed = current_timestamp - self._fuel_burned_timestamp
-        self._fuel_burned += (fuel_consumption / 3600) * seconds_passed
-        self._fuel_burned_timestamp = current_timestamp
-
-    @property
-    def fuel_burned(self) -> float:
-        """Returns amount of fuel burned and resets counter."""
-        fuel_burned = self._fuel_burned
-        self._fuel_burned = 0
-        return fuel_burned
 
     @property
     def software(self) -> Optional[str]:
@@ -193,9 +152,67 @@ class EcoMAX(Device):
 
     Attributes:
         address -- device address
+        _fuel_burned -- amount of fuel burned in kilograms
+        _fuel_burned_timestamp -- timestamp of burned fuel value update
     """
 
     address = ECOMAX_ADDRESS
+    _fuel_burned: float = 0.0
+    _fuel_burned_timestamp: float = 0.0
+
+    def __init__(
+        self, data: Dict[str, Any] = None, parameters: Dict[str, List[int]] = None
+    ):
+        """Creates device instance.
+
+        Keyword arguments:
+            data -- device data
+            parameters -- editable parameters
+        """
+        self.__dict__["_fuel_burned"] = 0.0
+        self.__dict__["_fuel_burned_timestamp"] = time.time()
+        super().__init__(data, parameters)
+
+    def set_data(self, data: Dict[str, Any]) -> None:
+        """Sets device data.
+
+        Keyword arguments:
+            data -- immutable device attributes
+        """
+        if DATA_FUEL_CONSUMPTION in data:
+            self._set_fuel_burned(data[DATA_FUEL_CONSUMPTION])
+
+        self._set_boiler_control_parameter()
+        super().set_data(data)
+
+    def _set_boiler_control_parameter(self):
+        """Sets boiler control parameter from device data."""
+        if PARAM_BOILER_CONTROL in self._parameters and isinstance(
+            self._parameters[PARAM_BOILER_CONTROL], Parameter
+        ):
+            self._parameters[PARAM_BOILER_CONTROL].value = int(self.is_on)
+        else:
+            self._parameters[PARAM_BOILER_CONTROL] = Parameter(
+                name=PARAM_BOILER_CONTROL, value=int(self.is_on), min_=0, max_=1
+            )
+
+    def _set_fuel_burned(self, fuel_consumption: float) -> None:
+        """Sets amount of fuel burned since last message.
+
+        Keyword arguments:
+            fuel_consumption -- fuel flow in kg/h
+        """
+        current_timestamp = time.time()
+        seconds_passed = current_timestamp - self._fuel_burned_timestamp
+        self._fuel_burned += (fuel_consumption / 3600) * seconds_passed
+        self._fuel_burned_timestamp = current_timestamp
+
+    @property
+    def fuel_burned(self) -> float:
+        """Returns amount of fuel burned and resets counter."""
+        fuel_burned = self._fuel_burned
+        self._fuel_burned = 0
+        return fuel_burned
 
 
 class EcoSTER(Device):
