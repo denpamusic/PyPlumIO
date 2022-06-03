f2260c118c8be807c7a45149f3384fd1546316cf
denpamusic <denpa@denpa.pro>
2022-06-03 03:34:09 +0300

    Improve code coverage.

--- tests/frames/test_responses.py
+++ tests/frames/test_responses.py
@@ -1,19 +1,52 @@
 """Test PyPlumIO response frames."""
 
 from pyplumio.constants import BROADCAST_ADDRESS, ECONET_ADDRESS
+from pyplumio.data_types import Byte
 from pyplumio.frames import responses
 from pyplumio.helpers.network_info import (
     EthernetParameters,
     NetworkInfo,
     WirelessParameters,
 )
+from pyplumio.helpers.product_info import ProductInfo
 from pyplumio.helpers.version_info import VersionInfo
 
-_program_version_data = {"version": VersionInfo(software="1.0.0")}
 
+def test_responses_type() -> None:
+    """Test if response is instance of frame class."""
+    for response in (
+        responses.ProgramVersion,
+        responses.DeviceAvailable,
+        responses.UID,
+        responses.Password,
+        responses.BoilerParameters,
+        responses.MixerParameters,
+        responses.DataSchema,
+    ):
+        frame = response(recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS)
+        assert isinstance(frame, response)
+
+
+_program_version_data = {"version": VersionInfo(software="1.0.0")}
 _program_version_bytes = bytearray(
     b"\xFF\xFF\x05\x7A\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x56"
 )
+
+
+def test_program_version_create_message() -> None:
+    """Test creating message for program version response."""
+    frame = responses.ProgramVersion(
+        recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS, data=_program_version_data
+    )
+    assert frame.create_message() == _program_version_bytes
+
+
+def test_program_version_parse_message() -> None:
+    """Test parsing message for program version response."""
+    frame = responses.ProgramVersion(recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS)
+    frame.parse_message(_program_version_bytes)
+    assert frame.data == _program_version_data
+
 
 _device_available_data = {
     "network": NetworkInfo(
@@ -32,42 +65,11 @@ _device_available_data = {
         ),
     )
 }
-
 _device_available_bytes = bytearray(
     b"\x01\xC0\xA8\x01\x02\xFF\xFF\xFF\x00\xC0\xA8\x01\x01\x01"
     + b"\xC0\xA8\x02\x02\xFF\xFF\xFF\x00\xC0\xA8\x02\x01\x01\x01\x64\x01\x00\x00"
     + b"\x00\x00\x05\x74\x65\x73\x74\x73"
 )
-
-
-def test_responses_type() -> None:
-    """Test if response is instance of frame class."""
-    for response in (
-        responses.ProgramVersion,
-        responses.DeviceAvailable,
-        responses.UID,
-        responses.Password,
-        responses.BoilerParameters,
-        responses.MixerParameters,
-        responses.DataSchema,
-    ):
-        frame = response(recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS)
-        assert isinstance(frame, response)
-
-
-def test_program_version_create_message() -> None:
-    """Test creating message for program version response."""
-    frame = responses.ProgramVersion(
-        recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS, data=_program_version_data
-    )
-    assert frame.create_message() == _program_version_bytes
-
-
-def test_program_version_parse_message() -> None:
-    """Test parsing message for program version response."""
-    frame = responses.ProgramVersion(recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS)
-    frame.parse_message(_program_version_bytes)
-    assert frame.data == _program_version_data
 
 
 def test_device_available_create_message() -> None:
@@ -84,5 +86,56 @@ def test_device_available_parse_message() -> None:
         recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS
     )
     frame.parse_message(_device_available_bytes)
-    print(frame.data)
     assert frame.data == _device_available_data
+
+
+_uid_data = {
+    "product": ProductInfo(
+        type=0,
+        product=90,
+        uid="D251PAKR3GCPZ1K8G05G0",
+        logo=23040,
+        image=2816,
+        model="EM350P2-ZF",
+    )
+}
+_uid_bytes = bytearray(
+    b"\x00Z\x00\x0b\x00\x16\x00\x11\r8386U9Z\x00\x00\x00\nEM350P2-ZF"
+)
+
+
+def test_uid_parse_message() -> None:
+    """Test parsing message for uid response."""
+    frame = responses.UID(recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS)
+    frame.parse_message(_uid_bytes)
+    assert frame.data == _uid_data
+
+
+_password_data = {"password": "0000"}
+_password_bytes = bytearray(b"\x040000")
+
+
+def test_password_parse_message() -> None:
+    """Test parsing message for password response."""
+    frame = responses.Password(recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS)
+    frame.parse_message(_password_bytes)
+    assert frame.data == _password_data
+
+
+_data_schema_bytes = bytearray(b"\x01\x00\x04\x00\x07")
+_data_schema_bytes_empty = bytearray(b"\x00\x00")
+_data_schema_data = [("mode", Byte())]
+
+
+def test_data_schema_parse_message() -> None:
+    """Test parsing message for data schema response."""
+    frame = responses.DataSchema(recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS)
+    frame.parse_message(_data_schema_bytes)
+    assert frame.data == _data_schema_data
+
+
+def test_data_schema_parse_message_with_no_parameters() -> None:
+    """Test parsing message for data schema with no parameters."""
+    frame = responses.DataSchema(recipient=BROADCAST_ADDRESS, sender=ECONET_ADDRESS)
+    frame.parse_message(_data_schema_bytes_empty)
+    assert frame.data == []
