65fa1a1aff47ea42df5cda4b89cd5767175c4401
denpamusic <denpa@denpa.pro>
2022-06-19 08:15:16 +0300

    Add tests for requests w/o data.

--- tests/frames/test_requests.py
+++ tests/frames/test_requests.py
@@ -1,6 +1,9 @@
 """Test PyPlumIO request frames."""
 
+import pytest
+
 from pyplumio.constants import BROADCAST_ADDRESS, ECONET_ADDRESS
+from pyplumio.exceptions import FrameDataError
 from pyplumio.frames import requests, responses
 
 
@@ -52,6 +55,12 @@ def test_set_parameter() -> None:
     assert frame.bytes == b"\x68\x0c\x00\x00\x56\x30\x05\x33\x00\x50\x64\x16"
 
 
+def test_set_parameter_with_no_data() -> None:
+    """Test set parameter request with no data."""
+    with pytest.raises(FrameDataError):
+        requests.SetBoilerParameter()
+
+
 def test_set_mixer_parameter() -> None:
     """Test set mixer parameter request bytes."""
     frame = requests.SetMixerParameter(
@@ -60,7 +69,19 @@ def test_set_mixer_parameter() -> None:
     assert frame.bytes == b"\x68\x0d\x00\x00\x56\x30\x05\x34\x00\x00\x28\x1a\x16"
 
 
+def test_set_mixer_parameter_with_no_data() -> None:
+    """Test set mixer parameter request with no data."""
+    with pytest.raises(FrameDataError):
+        requests.SetMixerParameter()
+
+
 def test_boiler_control() -> None:
     """Test boiler control parameter request bytes."""
     frame = requests.BoilerControl(data={"value": 1})
     assert frame.bytes == b"\x68\x0b\x00\x00\x56\x30\x05\x3b\x01\x3a\x16"
+
+
+def test_boiler_control_with_no_data() -> None:
+    """Test boiler control request with no data."""
+    with pytest.raises(FrameDataError):
+        requests.BoilerControl()
