7ca8f3a6fd3c235e7db966c502cbf334450870c6
denpamusic <denpa@denpa.pro>
2022-06-19 05:43:26 +0300

    Add no mixers test case.

--- tests/structures/test_mixers.py
+++ tests/structures/test_mixers.py
@@ -8,13 +8,21 @@ from pyplumio.structures.mixers import (
     from_bytes,
 )
 
-_message = bytearray.fromhex("0200002042260808000000204226080800")
+_message = bytearray.fromhex("0000002042260808000000204226080800")
+_message_zero_mixers = bytearray.fromhex("00")
 _data = {
     DATA_MIXERS: [
         {MIXER_TEMP: 40.0, MIXER_TARGET: 38, MIXER_PUMP: False},
         {MIXER_TEMP: 40.0, MIXER_TARGET: 38, MIXER_PUMP: False},
     ]
 }
+
+
+def test_from_bytes_with_zero_mixers():
+    """Test conversion from bytes with zero mixers."""
+    data, offset = from_bytes(_message_zero_mixers)
+    assert data == {}
+    assert offset == 1
 
 
 def test_from_bytes() -> None:
