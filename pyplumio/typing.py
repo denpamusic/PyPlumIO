f3d61c309608bbee4c37ac2e69d18c32fb3366d6
denpamusic <denpa@denpa.pro>
2022-06-25 01:45:14 +0300

    Fix incorrect pylint message.

--- pyplumio/typing.py
+++ pyplumio/typing.py
@@ -1,7 +1,6 @@
 """Contains custom typing."""
 
-from collections.abc import Awaitable, Callable
-from typing import Any, Tuple, Union
+from typing import Any, Awaitable, Callable, Tuple, Union
 
 ParameterTuple = Tuple[str, int, int, int]
 Numeric = Union[int, float]
