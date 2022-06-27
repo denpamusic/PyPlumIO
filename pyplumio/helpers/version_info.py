46b9e1abed48fe8cec1d2898dfff831362a1d8ef
denpamusic <denpa@denpa.pro>
2022-06-28 01:09:20 +0300

    Add version formatter.

--- pyplumio/helpers/version_info.py
+++ pyplumio/helpers/version_info.py
@@ -6,11 +6,17 @@ from dataclasses import dataclass
 from pyplumio.version import __version__
 
 
+def _formated_version(version=__version__) -> str:
+    """Format version for program version response."""
+    version_parts = version.split(".")
+    return ".".join(version_parts[0:3])
+
+
 @dataclass
 class VersionInfo:
     """Represents version info provided in program version response."""
 
-    software: str = __version__
+    software: str = _formated_version()
     struct_tag: bytes = b"\xFF\xFF"
     struct_version: int = 5
     device_id: bytes = b"\x7A\x00"
