5bc82b2084698d80bc5fb804b13bead217f0f604
denpamusic <denpa@denpa.pro>
2022-06-28 02:48:50 +0300

    Add connection helpers.

--- pyplumio/__init__.py
+++ pyplumio/__init__.py
@@ -4,8 +4,21 @@ from __future__ import annotations
 from pyplumio.connection import SerialConnection, TcpConnection
 from pyplumio.version import __version__
 
+
+def open_serial_connection(*args, **kwargs) -> SerialConnection:
+    """Helper function for Serial connection."""
+    return SerialConnection(*args, **kwargs)
+
+
+def open_tcp_connection(*args, **kwargs) -> TcpConnection:
+    """Helper function for TCP connection."""
+    return TcpConnection(*args, **kwargs)
+
+
 __all__ = [
     "SerialConnection",
     "TcpConnection",
+    "open_serial_connection",
+    "open_tcp_connection",
     "__version__",
 ]
--- pyplumio/__main__.py
+++ pyplumio/__main__.py
@@ -3,13 +3,12 @@ from __future__ import annotations
 
 import asyncio
 
-from pyplumio import SerialConnection
+import pyplumio
 
 
 async def main() -> None:
     """Connect and print out device sensors and parameters."""
-    async with SerialConnection(device="/dev/ttyUSB0", baudrate=115200) as connection:
-        await connection.connect()
+    async with pyplumio.open_serial_connection("/dev/ttyUSB0", 115200) as connection:
         device = connection.wait_for_device("ecomax")
         sensors = await device.get_value("sensors")
         parameters = await device.get_value("parameters")
