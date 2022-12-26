c3f11f791088cecc2b73c572befa5cc0efd0aa6e
denpamusic <denpa@denpa.pro>
2022-12-27 02:19:43 +0300

    Simplify regdata decoder.

--- pyplumio/structures/regulator_data.py
+++ pyplumio/structures/regulator_data.py
@@ -5,35 +5,44 @@ from typing import Final, List, Optional, Tuple
 
 from pyplumio.const import ATTR_REGDATA, ATTR_SCHEMA
 from pyplumio.helpers.data_types import Boolean, DataType
-from pyplumio.helpers.typing import DeviceDataType
+from pyplumio.helpers.typing import BytesType, DeviceDataType
 from pyplumio.structures import StructureDecoder, ensure_device_data
 from pyplumio.structures.frame_versions import FrameVersionsStructure
 
 REGDATA_VERSION: Final = "1.0"
 
 
+def _unpack_data(
+    data_type: DataType, data: BytesType, boolean_index: int = 0
+) -> Tuple[DataType, int]:
+    """Unpack data into the data type."""
+    data_type.unpack(data)
+    if isinstance(data_type, Boolean):
+        return data_type, data_type.index(boolean_index)
+
+    return data_type, boolean_index
+
+
 def _decode_regulator_data(
-    message: bytearray, offset: int, schema: List[Tuple[str, DataType]]
+    message: bytearray,
+    offset: int,
+    schema: List[Tuple[str, DataType]],
+    boolean_index: int = 0,
 ) -> DeviceDataType:
     """Decode regulator data from the schema."""
-    regulator_data: DeviceDataType = {}
-    boolean_index = 0
-    for parameter in schema:
-        parameter_id, parameter_type = parameter
-
-        if not isinstance(parameter_type, Boolean) and boolean_index > 0:
+    data = ensure_device_data(None)
+    for (sensor_id, data_type) in schema:
+        if not isinstance(data_type, Boolean) and boolean_index > 0:
             offset += 1
             boolean_index = 0
 
-        parameter_type.unpack(message[offset:])
-        if isinstance(parameter_type, Boolean):
-            boolean_index = parameter_type.index(boolean_index)
+        data_type, boolean_index = _unpack_data(
+            data_type, message[offset:], boolean_index
+        )
+        data[sensor_id] = data_type.value
+        offset += data_type.size
 
-        regulator_data[parameter_id] = parameter_type.value
-
-        offset += parameter_type.size
-
-    return regulator_data
+    return data
 
 
 class RegulatorDataStructure(StructureDecoder):
