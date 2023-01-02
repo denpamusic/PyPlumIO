04f3bfffb6a31c99411f5ffa7f57f6b813d8e912
denpamusic <denpa@denpa.pro>
2023-01-02 22:52:29 +0300

    Map frame type with enum.

--- pyplumio/structures/frame_versions.py
+++ pyplumio/structures/frame_versions.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 from typing import Final, Optional, Tuple
 
 from pyplumio import util
+from pyplumio.const import FrameType
 from pyplumio.helpers.typing import DeviceDataType, VersionsInfoType
 from pyplumio.structures import StructureDecoder, ensure_device_data
 
@@ -18,10 +19,15 @@ class FrameVersionsStructure(StructureDecoder):
     ) -> Tuple[DeviceDataType, int]:
         """Decode bytes and return message data and offset."""
         frame_versions: VersionsInfoType = {}
-        frames_number = message[offset]
+        frame_count = message[offset]
         offset += 1
-        for _ in range(frames_number):
+        for _ in range(frame_count):
             frame_type = message[offset]
+            try:
+                frame_type = FrameType(frame_type)
+            except ValueError:
+                pass
+
             version = util.unpack_ushort(message[offset + 1 : offset + 3])
             frame_versions[frame_type] = version
             offset += 3
