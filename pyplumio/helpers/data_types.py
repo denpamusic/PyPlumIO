c9944eda5399488594aaac3f80be4b1f00aa364a
denpamusic <denpa@denpa.pro>
2024-08-03 09:37:09 +0300

    Improve typing.
    Improved typing in data_types module by declaring DataType class as generic.

--- pyplumio/helpers/data_types.py
+++ pyplumio/helpers/data_types.py
@@ -5,36 +5,58 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 import socket
 import struct
-from typing import Any, ClassVar, Final, TypeVar
+from typing import ClassVar, Final, Generic, TypeVar
+
+T = TypeVar("T")
+DataTypeT = TypeVar("DataTypeT", bound="DataType")
 
 
-class DataType(ABC):
+class DataType(ABC, Generic[T]):
     """Represents a base data type."""
 
     __slots__ = ("_value", "_size")
 
-    _value: Any
+    _value: T
     _size: int
 
-    def __init__(self, value: Any = None):
+    def __init__(self, value: T | None = None):
         """Initialize a new data type."""
-        self._value = value
+        if value is not None:
+            self._value = value
+
         self._size = 0
 
     def __repr__(self) -> str:
         """Return serializable string representation of the class."""
-        return f"{self.__class__.__name__}(value={self._value})"
+        if hasattr(self, "_value"):
+            return f"{self.__class__.__name__}(value={self._value})"
+
+        return f"{self.__class__.__name__}()"
 
     def __eq__(self, other: object) -> bool:
         """Compare if this data type is equal to other."""
-        if isinstance(other, DataType):
+        if (
+            isinstance(other, DataType)
+            and hasattr(other, "_value")
+            and hasattr(self, "_value")
+        ):
             return bool(self._value == other._value)
 
-        return bool(self._value == other)
+        if (
+            isinstance(other, DataType)
+            and not hasattr(other, "_value")
+            and not hasattr(self, "_value")
+        ):
+            return type(self) is type(other)
+
+        if hasattr(self, "_value"):
+            return bool(self._value == other)
+
+        return NotImplemented
 
     def _slice_data(self, data: bytes) -> bytes:
         """Slice the data to data type size."""
-        return data[: self.size] if self.size is not None else data
+        return data if self.size == 0 else data[: self.size]
 
     @classmethod
     def from_bytes(cls: type[DataTypeT], data: bytes, offset: int = 0) -> DataTypeT:
@@ -48,7 +70,7 @@ class DataType(ABC):
         return self.pack()
 
     @property
-    def value(self) -> Any:
+    def value(self) -> T:
         """Return the data type value."""
         return self._value
 
@@ -64,9 +86,6 @@ class DataType(ABC):
     @abstractmethod
     def unpack(self, data: bytes) -> None:
         """Unpack the data."""
-
-
-DataTypeT = TypeVar("DataTypeT", bound=DataType)
 
 
 class Undefined(DataType):
@@ -86,21 +105,26 @@ class Undefined(DataType):
 BITARRAY_LAST_INDEX: Final = 7
 
 
-class BitArray(DataType):
+class BitArray(DataType[int]):
     """Represents a bit array."""
 
     __slots__ = ("_index",)
 
     _index: int
 
-    def __init__(self, value: Any = None, index: int = 0):
+    def __init__(self, value: bool | None = None, index: int = 0):
         """Initialize a new bit array."""
         super().__init__(value)
         self._index = index
 
     def __repr__(self) -> str:
         """Return serializable string representation of the class."""
-        return f"{self.__class__.__name__}(value={self._value}, index={self._index})"
+        if hasattr(self, "_value"):
+            return (
+                f"{self.__class__.__name__}(value={self._value}, index={self._index})"
+            )
+
+        return f"{self.__class__.__name__}(index={self._index})"
 
     def next(self, index: int = 0) -> int:
         """Set current bit and return the next index in the bit array."""
@@ -109,16 +133,22 @@ class BitArray(DataType):
 
     def pack(self) -> bytes:
         """Pack the data."""
-        return UnsignedChar(self._value).to_bytes()
+        if hasattr(self, "_value"):
+            return UnsignedChar(self._value).to_bytes()
+
+        return b""
 
     def unpack(self, data: bytes) -> None:
         """Unpack the data."""
         self._value = UnsignedChar.from_bytes(data[:1]).value
 
     @property
-    def value(self) -> bool | None:
+    def value(self) -> bool:
         """Return the data type value."""
-        return None if self._value is None else bool(self._value & (1 << self._index))
+        if hasattr(self, "_value"):
+            return bool(self._value & (1 << self._index))
+
+        return False
 
     @property
     def size(self) -> int:
@@ -126,7 +156,7 @@ class BitArray(DataType):
         return 1 if self._index == BITARRAY_LAST_INDEX else 0
 
 
-class IPv4(DataType):
+class IPv4(DataType[str]):
     """Represents an IPv4 address."""
 
     __slots__ = ()
@@ -145,7 +175,7 @@ class IPv4(DataType):
         self._value = socket.inet_ntoa(self._slice_data(data))
 
 
-class IPv6(DataType):
+class IPv6(DataType[str]):
     """Represents an IPv6 address."""
 
     __slots__ = ()
@@ -164,19 +194,19 @@ class IPv6(DataType):
         self._value = socket.inet_ntop(socket.AF_INET6, self._slice_data(data))
 
 
-class String(DataType):
+class String(DataType[str]):
     """Represents a null terminated string."""
 
     __slots__ = ()
 
-    def __init__(self, value: Any = ""):
+    def __init__(self, value: str = ""):
         """Initialize a new null terminated string data type."""
         super().__init__(value)
         self._size = len(self.value) + 1
 
     def pack(self) -> bytes:
         """Pack the data."""
-        value: str = self.value
+        value = self.value
         return value.encode() + b"\0"
 
     def unpack(self, data: bytes) -> None:
@@ -185,19 +215,19 @@ class String(DataType):
         self._size = len(self.value) + 1
 
 
-class VarBytes(DataType):
+class VarBytes(DataType[bytes]):
     """Represents a variable length bytes."""
 
     __slots__ = ()
 
-    def __init__(self, value: Any = b""):
+    def __init__(self, value: bytes = b""):
         """Initialize a new variable length bytes data type."""
         super().__init__(value)
         self._size = len(value) + 1
 
     def pack(self) -> bytes:
         """Pack the data."""
-        value: bytes = self.value
+        value = self.value
         return UnsignedChar(self.size - 1).to_bytes() + value
 
     def unpack(self, data: bytes) -> None:
@@ -206,23 +236,28 @@ class VarBytes(DataType):
         self._value = data[1 : self.size]
 
 
-class VarString(VarBytes):
+class VarString(DataType[str]):
     """Represents a variable length string."""
 
     __slots__ = ()
 
+    def __init__(self, value: str = ""):
+        """Initialize a new variable length bytes data type."""
+        super().__init__(value)
+        self._size = len(value) + 1
+
     def pack(self) -> bytes:
         """Pack the data."""
-        value: str = self.value
+        value = self.value
         return UnsignedChar(self.size - 1).to_bytes() + value.encode()
 
     def unpack(self, data: bytes) -> None:
         """Unpack the data."""
-        super().unpack(data)
-        self._value = self.value.decode()
+        self._size = data[0] + 1
+        self._value = data[1 : self.size].decode("utf-8", "replace")
 
 
-class BuiltInDataType(DataType, ABC):
+class BuiltInDataType(DataType[T], ABC):
     """Represents a data type that is supported by the struct module."""
 
     __slots__ = ()
@@ -243,7 +278,7 @@ class BuiltInDataType(DataType, ABC):
         return self._struct.size
 
 
-class SignedChar(BuiltInDataType):
+class SignedChar(BuiltInDataType[int]):
     """Represents a signed char."""
 
     __slots__ = ()
@@ -251,7 +286,7 @@ class SignedChar(BuiltInDataType):
     _struct: ClassVar[struct.Struct] = struct.Struct("<b")
 
 
-class UnsignedChar(BuiltInDataType):
+class UnsignedChar(BuiltInDataType[int]):
     """Represents an unsigned char."""
 
     __slots__ = ()
@@ -259,7 +294,7 @@ class UnsignedChar(BuiltInDataType):
     _struct: ClassVar[struct.Struct] = struct.Struct("<B")
 
 
-class Short(BuiltInDataType):
+class Short(BuiltInDataType[int]):
     """Represents a 16 bit integer."""
 
     __slots__ = ()
@@ -267,7 +302,7 @@ class Short(BuiltInDataType):
     _struct: ClassVar[struct.Struct] = struct.Struct("<h")
 
 
-class UnsignedShort(BuiltInDataType):
+class UnsignedShort(BuiltInDataType[int]):
     """Represents an unsigned 16 bit integer."""
 
     __slots__ = ()
@@ -275,7 +310,7 @@ class UnsignedShort(BuiltInDataType):
     _struct: ClassVar[struct.Struct] = struct.Struct("<H")
 
 
-class Int(BuiltInDataType):
+class Int(BuiltInDataType[int]):
     """Represents a 32 bit integer."""
 
     __slots__ = ()
@@ -283,7 +318,7 @@ class Int(BuiltInDataType):
     _struct: ClassVar[struct.Struct] = struct.Struct("<i")
 
 
-class UnsignedInt(BuiltInDataType):
+class UnsignedInt(BuiltInDataType[int]):
     """Represents a unsigned 32 bit integer."""
 
     __slots__ = ()
@@ -291,7 +326,7 @@ class UnsignedInt(BuiltInDataType):
     _struct: ClassVar[struct.Struct] = struct.Struct("<I")
 
 
-class Float(BuiltInDataType):
+class Float(BuiltInDataType[int]):
     """Represents a float."""
 
     __slots__ = ()
@@ -299,7 +334,7 @@ class Float(BuiltInDataType):
     _struct: ClassVar[struct.Struct] = struct.Struct("<f")
 
 
-class Double(BuiltInDataType):
+class Double(BuiltInDataType[int]):
     """Represents a double."""
 
     __slots__ = ()
@@ -307,7 +342,7 @@ class Double(BuiltInDataType):
     _struct: ClassVar[struct.Struct] = struct.Struct("<d")
 
 
-class Int64(BuiltInDataType):
+class Int64(BuiltInDataType[int]):
     """Represents a 64 bit signed integer."""
 
     __slots__ = ()
@@ -315,7 +350,7 @@ class Int64(BuiltInDataType):
     _struct: ClassVar[struct.Struct] = struct.Struct("<q")
 
 
-class UInt64(BuiltInDataType):
+class UInt64(BuiltInDataType[int]):
     """Represents a 64 bit unsigned integer."""
 
     __slots__ = ()
