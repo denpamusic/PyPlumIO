3618399af4e97c1365dda2c2e2262082c172878a
denpamusic <denpa@denpa.pro>
2025-02-22 03:46:00 +0300

    Update annotation.

--- tests/helpers/test_schedule.py
+++ tests/helpers/test_schedule.py
@@ -25,7 +25,7 @@ from pyplumio.structures.schedules import (
 
 @pytest.fixture(name="intervals")
 def fixture_intervals() -> list[Time]:
-    """Return example schedule for a day."""
+    """Return time intervals for a schedule day."""
     return (
         "00:00, 00:30, 01:00, 01:30, 02:00, 02:30, 03:00, 03:30, 04:00, 04:30, 05:00, "
         "05:30, 06:00, 06:30, 07:00, 07:30, 08:00, 08:30, 09:00, 09:30, 10:00, 10:30, "
