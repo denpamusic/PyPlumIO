dbd9bbd42c60f5b4b92544b2972ac50eea6a3f60
denpamusic <denpa@denpa.pro>
2025-05-07 04:14:08 +0300

    Rework connection tests.

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,28 +1,27 @@
 """Contains tests for the connection classes."""
 
+from __future__ import annotations
+
 from asyncio import StreamReader, StreamWriter
 from importlib import reload
 import logging
 import sys
 from typing import Final
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock, Mock, patch
 
 import pytest
-from serial import EIGHTBITS, PARITY_NONE, STOPBITS_ONE, SerialException
+from serial import EIGHTBITS, PARITY_NONE, STOPBITS_ONE
 
-import pyplumio.connection
-from pyplumio.devices import PhysicalDevice
+from pyplumio.connection import Connection, SerialConnection, TcpConnection
 from pyplumio.exceptions import ConnectionFailedError
 from pyplumio.protocol import Protocol
-
-HOST: Final = "localhost"
-PORT: Final = 8899
-DEVICE: Final = "/dev/ttyUSB0"
 
 
 @pytest.fixture(name="use_fast", params=(True, False))
 def fixture_use_fast(request, monkeypatch, caplog):
     """Try with and without serial-asyncio-fast package."""
+    import pyplumio.connection
+
     if not request.param:
         monkeypatch.setitem(sys.modules, "serial_asyncio_fast", None)
 
@@ -79,169 +78,147 @@ def fixture_serial_asyncio_open_serial_connection(
 @pytest.fixture(name="mock_protocol")
 def fixture_mock_protocol():
     """Return a mock protocol object."""
-    return AsyncMock(spec=Protocol)
+    return AsyncMock(spec=Protocol, autospec=True)
 
 
 @pytest.fixture(name="tcp_connection")
-def fixture_tcp_connection(mock_protocol) -> pyplumio.connection.TcpConnection:
+def fixture_tcp_connection(mock_protocol) -> TcpConnection:
     """Return at TCP connection object."""
-    return pyplumio.connection.TcpConnection(
-        host=HOST, port=PORT, timeout=10, protocol=mock_protocol
-    )
+    return TcpConnection(host=HOST, port=PORT, timeout=10, protocol=mock_protocol)
 
 
 @pytest.fixture(name="serial_connection")
-def fixture_serial_connection(mock_protocol) -> pyplumio.connection.SerialConnection:
+def fixture_serial_connection(mock_protocol) -> SerialConnection:
     """Return a serial connection object."""
-    return pyplumio.connection.SerialConnection(
-        device="/dev/ttyUSB0", timeout=10, protocol=mock_protocol
+    return SerialConnection(device="/dev/ttyUSB0", timeout=10, protocol=mock_protocol)
+
+
+class DummyConnection(Connection):
+    """Represents a dummy connection for tests."""
+
+    async def _open_connection(self):
+        """Open the connection."""
+
+
+class TestConnection:
+    """Contains test for Connection class."""
+
+    @patch.object(
+        DummyConnection, "_open_connection", return_value=("reader", "writer")
     )
+    async def test_connect(self, mock_open_connection, mock_protocol) -> None:
+        """Test a connection."""
+        connection = DummyConnection(protocol=mock_protocol)
+        assert connection.protocol is mock_protocol
+        await connection.connect()
+        mock_open_connection.assert_awaited_once()
+        await connection.close()
+        mock_protocol.shutdown.assert_called_once()
+
+    @pytest.mark.parametrize(
+        ("reconnect_on_failure", "expected_awaits", "expected_log"),
+        [
+            (True, 2, "Can't connect to the device"),
+            (False, 1, None),
+        ],
+    )
+    @patch.object(DummyConnection, "_open_connection", side_effect=(OSError, None))
+    async def test_reconnect(
+        self,
+        mock_open_connection,
+        caplog,
+        reconnect_on_failure: bool,
+        expected_awaits: int,
+        expected_log: str | None,
+    ) -> None:
+        """Test reconnect logic."""
+        connection = DummyConnection(reconnect_on_failure=reconnect_on_failure)
+        if reconnect_on_failure:
+            with caplog.at_level(logging.ERROR):
+                await connection.connect()
+
+            assert expected_log in caplog.text
+        else:
+            with pytest.raises(ConnectionFailedError):
+                await connection.connect()
+
+        mock_open_connection.await_count == expected_awaits
+
+    @patch.object(DummyConnection, "close")
+    @patch.object(DummyConnection, "connect")
+    async def test_context_manager(self, mock_connect, mock_close) -> None:
+        """Test context manager."""
+        async with DummyConnection():
+            pass
+
+        mock_connect.assert_called_once()
+        mock_close.assert_called_once()
+
+    @patch.object(DummyConnection, "_connect")
+    async def test_connection_lost(self, mock_connect, mock_protocol) -> None:
+        """Test that connection lost callback calls reconnect."""
+        connection = DummyConnection(protocol=mock_protocol)
+        await connection.connect()
+        on_connection_lost = mock_protocol.on_connection_lost.add.call_args.args[0]
+        await on_connection_lost()
+        assert mock_connect.call_count == 2
+
+    async def test_getattr(self, mock_protocol) -> None:
+        """Test that getattr is getting proxied to the protocol."""
+        connection = DummyConnection(protocol=mock_protocol)
+        mock_protocol.some_attr = "value"
+        assert connection.some_attr == "value"
+        mock_protocol.some_method = Mock(return_value="called")
+        assert connection.some_method() == "called"
 
 
-async def test_tcp_connect(mock_protocol, asyncio_open_connection) -> None:
-    """Test TCP connection logic."""
-    with patch("pyplumio.connection.Connection._reconnect") as mock_reconnect:
-        tcp_connection = pyplumio.connection.TcpConnection(
-            host=HOST,
-            port=PORT,
-            timeout=10,
-            protocol=mock_protocol,
-            reconnect_on_failure=True,
+HOST: Final = "localhost"
+PORT: Final = 8899
+
+
+class TestTcpConnection:
+    """Contains tests for TcpConnection class."""
+
+    async def test_connection(
+        self, tcp_connection: TcpConnection, asyncio_open_connection
+    ) -> None:
+        """Test TCP connection."""
+        await tcp_connection.connect()
+        asyncio_open_connection.assert_awaited_once_with(
+            host=HOST, port=PORT, timeout=10
         )
 
-        assert tcp_connection.protocol == mock_protocol
-        mock_protocol.on_connection_lost.add.assert_called_once_with(mock_reconnect)
-
-    await tcp_connection.connect()
-    asyncio_open_connection.assert_called_once_with(host=HOST, port=PORT, timeout=10)
-    await tcp_connection.close()
-
-    # Raise custom exception on connection failure.
-    tcp_connection = pyplumio.connection.TcpConnection(
-        host=HOST,
-        port=PORT,
-        timeout=10,
-        protocol=mock_protocol,
-        reconnect_on_failure=False,
-    )
-
-    asyncio_open_connection.reset_mock()
-    asyncio_open_connection.side_effect = OSError
-    with pytest.raises(ConnectionFailedError):
-        await tcp_connection.connect()
+    async def test_repr(self, tcp_connection: TcpConnection) -> None:
+        """Test serializable representation."""
+        assert (
+            repr(tcp_connection)
+            == f"TcpConnection(host={HOST}, port={PORT}, kwargs={{'timeout': 10}})"
+        )
 
 
-async def test_serial_connect(
-    mock_protocol, serial_asyncio_open_serial_connection
-) -> None:
-    """Test a serial connection logic."""
-    serial_connection = pyplumio.connection.SerialConnection(
-        device=DEVICE, timeout=10, reconnect_on_failure=False, protocol=mock_protocol
-    )
-    await serial_connection.connect()
-    serial_asyncio_open_serial_connection.assert_called_once_with(
-        url=DEVICE,
-        baudrate=115200,
-        bytesize=EIGHTBITS,
-        parity=PARITY_NONE,
-        stopbits=STOPBITS_ONE,
-        timeout=10,
-    )
+DEVICE: Final = "/dev/ttyUSB0"
 
-    # Raise custom exception on connection failure.
-    serial_asyncio_open_serial_connection.reset_mock()
-    serial_asyncio_open_serial_connection.side_effect = SerialException
-    with pytest.raises(ConnectionFailedError):
+
+class TestSerialConnection:
+    """Contains tests for SerialConnection class."""
+
+    async def test_connect(
+        self, serial_connection: SerialConnection, serial_asyncio_open_serial_connection
+    ) -> None:
+        """Test serial connect."""
         await serial_connection.connect()
+        serial_asyncio_open_serial_connection.assert_called_once_with(
+            url=DEVICE,
+            baudrate=115200,
+            bytesize=EIGHTBITS,
+            parity=PARITY_NONE,
+            stopbits=STOPBITS_ONE,
+            timeout=10,
+        )
 
-
-@patch(
-    "pyplumio.connection.Connection._connect", side_effect=(ConnectionFailedError, None)
-)
-@pytest.mark.usefixtures("asyncio_open_connection")
-async def test_reconnect(
-    mock_connect, tcp_connection: pyplumio.connection.TcpConnection, caplog
-) -> None:
-    """Test a reconnect logic."""
-    with caplog.at_level(logging.ERROR):
-        await tcp_connection.connect()
-        await tcp_connection.wait_until_done()
-
-    assert "Can't connect to the device" in caplog.text
-    assert mock_connect.call_count == 2
-
-
-@pytest.mark.usefixtures("asyncio_open_connection")
-async def test_connection_lost(
-    mock_protocol, tcp_connection: pyplumio.connection.TcpConnection
-) -> None:
-    """Test that connection lost callback calls reconnect."""
-    await tcp_connection.connect()
-    on_connection_lost = mock_protocol.on_connection_lost.add.call_args.args[0]
-    with patch("pyplumio.connection.Connection._connect") as mock_connect:
-        await on_connection_lost()
-        mock_connect.assert_called_once()
-
-
-async def test_reconnect_logic_selection() -> None:
-    """Test reconnect logic selection."""
-    connection = pyplumio.connection.TcpConnection(
-        host=HOST, port=PORT, reconnect_on_failure=False
-    )
-
-    with (
-        patch("pyplumio.connection.Connection._connect") as mock_connect,
-        patch("pyplumio.connection.Connection._reconnect") as mock_reconnect,
-    ):
-        await connection.connect()
-
-    mock_reconnect.assert_not_called()
-    mock_connect.assert_called_once()
-
-
-@patch("pyplumio.connection.Connection.close")
-@patch("pyplumio.connection.Connection.connect")
-async def test_context_manager(mock_connect, mock_close) -> None:
-    """Test context manager integration."""
-    async with pyplumio.connection.TcpConnection(host=HOST, port=PORT) as connection:
-        assert isinstance(connection, pyplumio.connection.TcpConnection)
-        assert connection.host == HOST
-        assert connection.port == PORT
-
-    mock_connect.assert_called_once()
-    mock_close.assert_called_once()
-
-
-@pytest.mark.usefixtures("asyncio_open_connection")
-async def test_getattr(
-    mock_protocol, tcp_connection: pyplumio.connection.TcpConnection
-) -> None:
-    """Test that getattr is getting proxied to the protocol."""
-    mock_protocol.get_device = AsyncMock(spec=PhysicalDevice)
-    await tcp_connection.connect()
-    await tcp_connection.get_device("test")
-    mock_protocol.get_device.assert_called_once()
-
-
-@pytest.mark.usefixtures("asyncio_open_connection")
-async def test_close(
-    mock_protocol, tcp_connection: pyplumio.connection.TcpConnection
-) -> None:
-    """Test connection close."""
-    await tcp_connection.connect()
-    await tcp_connection.close()
-    mock_protocol.shutdown.assert_called_once()
-
-
-async def test_repr(
-    tcp_connection: pyplumio.connection.TcpConnection,
-    serial_connection: pyplumio.connection.SerialConnection,
-) -> None:
-    """Test serializable representation."""
-    assert (
-        repr(tcp_connection)
-        == f"TcpConnection(host={HOST}, port={PORT}, kwargs={{'timeout': 10}})"
-    )
-    assert repr(serial_connection) == (
-        f"SerialConnection(device={DEVICE}, baudrate=115200, kwargs={{'timeout': 10}})"
-    )
+    async def test_repr(self, serial_connection: SerialConnection) -> None:
+        """Test serializable representation."""
+        assert repr(serial_connection) == (
+            "SerialConnection("
+            f"device={DEVICE}, baudrate=115200, kwargs={{'timeout': 10}})"
+        )
