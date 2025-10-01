80657d7da199bc50c2bf3c1c2252d76bf7ebb137
denpamusic <denpamusic@users.noreply.github.com>
2025-10-01 19:30:40 +0000

    deploy: e6a8c3bdcffcb9854c867af181c13155a95b2669

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.6.2.dev9+ge6a8c3bdc',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
